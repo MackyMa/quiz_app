85b4fa8663a3a066938bed89e74bb8aedf69c5fc
MackyMa <malaika.m99@gmail.com>
2025-09-25 00:29:38 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/Quiz.dart';
+//import 'package:quiz_app/questions_screen.dart';
+//import 'package:quiz_app/start_screen.dart';
+
+void main(){
+  runApp(
+    Quiz(),
+  );
+}
